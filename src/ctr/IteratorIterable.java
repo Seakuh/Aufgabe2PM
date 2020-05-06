9ca24c033314d69241ef4be4d197286d6d65bf47
Seakuh <eteranimal@protonmail.com>
2020-05-07 00:06:38 +0200

    Rümpfe gefüllt

--- src/ctr/IteratorIterable.java
+++ src/ctr/IteratorIterable.java
@@ -1,5 +1,50 @@
 package ctr;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-public class IteratorIterable {
+public class IteratorIterable implements Iterator<String>{
+
+	List<String> list = new LinkedList<String>();
+	String line = "";
+	BufferedReader br;
+	boolean isNext = false;
+	
+	public IteratorIterable(BufferedReader br) {
+		this.br = br;
+	}
+	
+	@Override
+	public boolean hasNext() {
+		if(isNext == false) {
+			try {
+				line = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			isNext = true;
+			return (line != null);
+		} else {
+			return (line != null);
+		}
+	}
+
+	@Override
+	public String next() {
+		if(isNext == false) {
+			try {
+				line = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			isNext = true;
+			return line;
+		} else {
+			isNext = false;
+			return line;
+		}
+	}
 
 }
