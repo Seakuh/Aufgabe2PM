1c3d27ffa52feb1e42ed9e72c2eb2caef315a058
Dizzle <Dizzle@DESKTOP-6D5OFKL>
2020-05-06 23:19:11 +0200

    Aufgabe 6 Kontrukt commit

---
+++ src/ctr/LineReader.java
@@ -0,0 +1,13 @@
+package ctr;
+
+import java.util.Iterator;
+
+public class LineReader implements Iterable<String> {
+
+        @Override
+        public Iterator<String> iterator() {
+                return null;
+        }
+
+
+}
