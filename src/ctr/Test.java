8283615ad85f5edf800cc199d1588e376deff6ce
Seakuh <eteranimal@protonmail.com>
2020-05-08 23:30:43 +0200

    tests added

--- src/ctr/Test.java
+++ src/ctr/Test.java
@@ -3,7 +3,9 @@ package ctr;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.io.Reader;
+import java.io.StringReader;
 
 public class Test {
 
@@ -16,6 +18,32 @@ public class Test {
 			System.out.println(line);
 		for (String line : lines)
 			System.out.println(line);
+		
+        String str = "ls"; 
+        
+        StringReader stringReader = new StringReader(str); 
+        
+        int ch = 0; 
+
+		
+		
+        for (int i = 0; i < 5; i++) { 
+            try {
+				ch = stringReader.read();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+            System.out.println("\nInteger value "
+                               + "of character read: "
+                               + ch); 
+            System.out.println("Actual "
+                               + "character read: "
+                               + (char)ch); 
+        } 
+		
+		
+		
 	}
 
 }
